4db8a917ba6936a4259d1eaf591a90ca91a1aebc
Vaskar Guchhait <vaskarguchhait2001@gmail.com>
2025-07-13 19:58:53 +0530

    Adding comment

--- src/main/java/com/student/StudentApplication.java
+++ src/main/java/com/student/StudentApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentApplication {
 
+	/**
+	 * This is the main class which is the starting point of my application
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApplication.class, args);
 	}
